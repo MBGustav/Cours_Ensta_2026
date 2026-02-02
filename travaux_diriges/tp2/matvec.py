e00dc2028a0dc974f83b3f38dbdf0b0661f0ff25
Gustavo Barreto <gbarreto@estudante.ufscar.br>
2026-02-02 23:33:02 +0100

    [OK] exercise 2A

--- travaux_diriges/tp2/matvec.py
+++ travaux_diriges/tp2/matvec.py
@@ -1,19 +1,24 @@
 # Produit matrice-vecteur v = A.u
+from time import time
 import numpy as np
 
 # Dimension du problème (peut-être changé)
-dim = 120
+dim = 320
 # Initialisation de la matrice (un peu plus explicite...)
 A = np.zeros((dim, dim))
+beg = time()
+
 for j in range(dim):
     for i in range(dim):
         A[j, i] = (i + j) % dim + 1.
-print(f"A = {A}")
+
 
 # Initialisation du vecteur u
 u = np.array([i+1. for i in range(dim)])
-print(f"u = {u}")
+
 
 # Produit matrice-vecteur
 v = A.dot(u)
-print(f"v = {v}")
+end = time()
+print(f"Temp Serial = {(end-beg)*1e3:.2f} ms")
+
